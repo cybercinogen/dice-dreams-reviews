87a21b1688c24803ad756619ac19f2ab11afe049
cybercinogen <116560209+cybercinogen@users.noreply.github.com>
2024-11-07 10:25:30 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 
+import os
 from flask import Flask, render_template, request
 from database import Session, Review
 from datetime import datetime, timedelta
@@ -44,4 +45,6 @@ def index():
     return render_template('index.html', **locals())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Use the PORT environment variable if available; otherwise, default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
