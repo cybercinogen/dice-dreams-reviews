1dee774fab6902a69be2787e6335b1e812f32a4e
cybercinogen <116560209+cybercinogen@users.noreply.github.com>
2024-11-07 12:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ def index():
     session.close()
     return render_template('index.html', **locals())
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
